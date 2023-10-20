bb1596b84494cbc514d33fda75ecb7726129ddeb
sayantan goswami <65398599+gsayant007@users.noreply.github.com>
2023-10-20 21:29:55 +0000

    commit #2.

--- rust_test_for_nvim/src/main.rs
+++ rust_test_for_nvim/src/main.rs
@@ -1,7 +1,11 @@
+mod weird_func_from_py;
+use weird_func_from_py::func;
 fn main() {
     println!("hello world!");
 
     for i in 0..10 {
         println!("{}", i);
     }
+    println!("{:?}",func(vec![1]));
+    func();
 }
---
+++ rust_test_for_nvim/src/weird_func_from_py.rs
@@ -0,0 +1,6 @@
+
+pub fn func(arg:&mut Vec<i32>)->Vec<i32>{
+    arg.push(1);
+    return arg.to_vec();
+}
+
