2a50640a9a363eba40180ce6584e1037ed4033e9
sayantan goswami <65398599+gsayant007@users.noreply.github.com>
2023-10-27 21:26:43 +0000

    commit to add rust unit tests.

--- rust_test_for_nvim/src/main.rs
+++ rust_test_for_nvim/src/main.rs
@@ -1,10 +1,22 @@
 mod weird_func_from_py;
-use weird_func_from_py::gcd;
+use weird_func_from_py::{gcd,gcs};
 fn main() {
     println!("hello world!");
-
-    for i in 0..10 {
-        println!("{}", i);
-    }
-    gcd(18, 12);
+    println!("{}",gcd(12, 18));
+    println!("{}",gcd(2 * 3 * 5 * 11 * 17,3 * 7 * 11 * 13 * 19));
+    gcs(7, 3);
 }
+
+#[test]
+fn test_gcd() {
+    println!("{:?}",assert_eq!(gcd(14, 15), 1));
+    println!("{:?}",assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
+        3 * 7 * 11 * 13 * 19), 3 * 11));
+   }
+
+// #[test]
+// fn test_gcs() {
+//     println!("{:?}",assert_eq!(gcd(14, 15), 1));
+//     println!("{:?}",assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
+//         3 * 7 * 11 * 13 * 19), 3 * 11));
+//     }
--- rust_test_for_nvim/src/weird_func_from_py.rs
+++ rust_test_for_nvim/src/weird_func_from_py.rs
@@ -3,26 +3,45 @@
 //     return arg.to_vec();
 // }
 
-pub fn gcd(mut a: i32, mut b: i32) {
-    // assert!(b > a);
+pub fn gcd(mut a: u32, mut b: u32)->u32 {
+    assert!(a>0 && b>0);
     if b > a {
         // let mut remainders_list = vec![];
-        let mut remainders_list = Vec::new();
+        // let mut remainders_list = Vec::new();
         let mut r = b % a;
-        if r == 1 {
-            println!("gcd is {}",r);
-        }
-        while r > 0 {
-            r = b % a;
-            remainders_list.push(r);
-            println!("{},{},{}", a, b, r);
+        // if r == 1 {
+        //     println!("gcd is {}",r);
+        // }
+        while r != 0 {
+            // remainders_list.push(r);
+            // println!("{},{},{}", a, b, r);
             b = a;
             a = r;
+            r = b % a;
         }
-        println!("the remiander list is {:?}",remainders_list);
+        // println!("the remainder list is {:?}",remainders_list);
+        // println!("{},{},{}", a, b, r);
+        // println!("gcd is {}",a);
+        a
     }
     else {
-        gcd(b, a);
+        gcd(b, a)
     }
-
 }
+
+pub fn gcs(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+    if m < n {
+    let t = m;
+    m = n;
+    n = t;
+    }
+    m = m % n;
+    }
+    println!("gcs is {}",n);
+    n
+   }
+
+   
+
