b3cfb11e1c845370b7d35502bf92a8ab4f8297ab
gsayantan9 <158445355+gsayantan9@users.noreply.github.com>
2024-02-17 17:35:04 +0000

    web scraping through computer vision.

--- web-scraping/web_scraping_test1.py
+++ web-scraping/web_scraping_test1.py
@@ -1,15 +1,36 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
+# import asyncio
+# from playwright.async_api import async_playwright, Playwright
 
-url = "http://olympus.realpython.org/profiles/dionysus"
+# async def run(playwright: Playwright):
+#     iphone_13 = playwright.devices['iPhone 13']
+#     browser = await playwright.webkit.launch(headless=False)
+#     # context = await browser.new_context(
+#     #     **iphone_13,
+#     context = await browser.new_context(geolocation={"longitude": 41.890221, "latitude": 12.492348},
+#                                         permissions=["geolocation"]
+#   )
 
-page = urlopen(url=url)
+# async def main():
+#     async with async_playwright() as playwright:
+#         await run(playwright)
+# asyncio.run(main())
+import cv2
+import easyocr
+import matplotlib.pyplot as plt
+from selenium import webdriver
 
-html = page.read().decode("utf-8")
+# part_number = "49058"
+# page_url = f"https://shop.advanceautoparts.com/web/SearchResults?searchTerm={part_number}&storeId=10151&catalogId=10051&langId=-1"
+# browser = webdriver.ChromeOptions()
+# browser.add_argument('--headless')
+# driver = webdriver.Chrome(options=browser)
+# driver.get(page_url)
+# driver.save_screenshot(f"{part_number}.jpeg")
+image_path = "/workspaces/tkinter/web-scraping/advance_auto_49058.jpeg"
+img = cv2.imread(image_path)
+reader = easyocr.Reader(["en"],gpu=False)
+text_list = reader.readtext(img)
+for t in text_list:
+    print(t)
 
-soup = BeautifulSoup(html,"html.parser")
-
-print(soup)
-print(soup.get_text().strip("\n"))
-
-
+      
